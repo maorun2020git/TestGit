7dbe72e7affc094fa0fecd2dc74f888c23b7d6d3
Administrator <Administrator@G5Z5OV6POAFKRFG>
2020-02-04 22:03:53 +0800

    ok TestGit My First commit

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.atguigu.git;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Apple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("sssssssssss");
+
+	}
+
+}
